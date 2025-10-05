8948aafaabbeec8871efe41ff23abdc76908d93e
Santiago Ramos <santiago.ramos@oei.int>
2025-10-05 11:17:20 +0200

    Section name

--- app/helpers/old_application_helper.rb
+++ app/helpers/old_application_helper.rb
@@ -310,11 +310,11 @@ module OldApplicationHelper
   # Devuelve las secciones disponibles para el usuario
   def menu_secciones user=nil
     sections = {
-    sales: {url: client_notes_path, label: 'Caja'},
-    products: {url: '/products/products', label: 'Productos'},
-    accounting: {url: '/accounting/caja', label: 'Tesorería'},
-    #distribution: {url: '/editor/products', label: 'Distribuidora'},
-    admin: {url: '/admin/avisos', label: 'Administración'}
+      sales: {url: client_notes_path, label: 'Caja'},
+      products: {url: '/products/products', label: 'Productos'},
+      accounting: {url: '/accounting/caja', label: 'Tesorería'},
+      #distribution: {url: '/editor/products', label: 'Distribuidora'},
+      admin: {url: '/admin/avisos', label: 'Administración'}
     }
     if user && user.class.name == "User"
       sections = sections.select{|k, v| user.granted?(k) }
